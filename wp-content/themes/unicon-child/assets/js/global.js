f0e2e4bb9dcb7ce0512c0840edb3c5d4d28f6b8e
Anatoliy <lightduelist@gmail.com>
2016-12-20 15:24:09 +0200

    #3921

--- wp-content/themes/unicon-child/assets/js/global.js
+++ wp-content/themes/unicon-child/assets/js/global.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function($){
+
+  var homepage_slider = {
+    $slider: $('.rev_slider_wrapper.fullwidthbanner-container'),
+    slides: null,
+    init: function () {
+      var self = this;
+      if (self.$slider.length > 0) {
+        self.slides = self.$slider.find('ul>li').length;
+        if (self.slides == 1) {
+          self.$slider.addClass('only-one-slide');
+        }
+      }
+    }
+  }.init();
+
   var event_index = {
     $select_element: $('.tribe-bar-search-category-filter>select'),
     init: function(){
