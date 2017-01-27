49c382f670584438ec45b49c26f3adda123dc73f
Anatoliy <lightduelist@gmail.com>
2017-01-27 14:49:48 +0200

    #GSWA-31

--- wp-content/themes/unicon-child/searchform.php
+++ wp-content/themes/unicon-child/searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-    <input type="text" value="" placeholder="Search GSWA" name="s" id="s"/>
+    <input type="text" value="<?php print (empty($_REQUEST['s'])?NULL:$_REQUEST['s']) ?>" placeholder="Search GSWA" name="s" id="s"/>
     <input type="submit" id="searchsubmit" value="Search"/>
 </form>
