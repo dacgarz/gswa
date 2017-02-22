43d08b165f1ec5f170ad3e7d11125dda31a4ee63
Anatoliy <lightduelist@gmail.com>
2017-02-22 11:01:21 +0200

    #publications

--- wp-content/themes/unicon-child/functions.php
+++ wp-content/themes/unicon-child/functions.php
@@ -15,7 +15,7 @@ function theme_enqueue_styles() {
   wp_enqueue_style( 'unicon_child_header_css', get_stylesheet_directory_uri() . '/framework/css/header.css', array(), '0.0.1');
   wp_enqueue_style( 'unicon_child_footer_css', get_stylesheet_directory_uri() . '/framework/css/footer.css');
 
-  wp_enqueue_style( 'unicon_child_styles_gulp', get_stylesheet_directory_uri() . '/assets/compiled/css/global.css', array(), '0.0.6');
+  wp_enqueue_style( 'unicon_child_styles_gulp', get_stylesheet_directory_uri() . '/assets/compiled/css/global.css', array(), '0.0.7');
   wp_enqueue_script( 'unicon_child_js_gulp', get_stylesheet_directory_uri() . '/assets/compiled/js/global.js', array('jquery'), '0.0.4');
 }
 
